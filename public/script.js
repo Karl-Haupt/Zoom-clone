7ecbfa5fa2ed99d1b1fe5f057a4a51247dadf2a8
Karl-Haupt <karlhaupt1@gmail.com>
2022-01-13 17:19:37 +0200

    Finshed Zoom Clone || Play Button/Mute Button bug

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
+const socket = io('/');
+
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+  })
 
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
@@ -9,13 +17,104 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        });
+    })
+
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    });
+
+    //Messaging/Chat
+    let text = $('input');
+
+    $('html').keydown(e => {
+        if(e.which == 13 && text.val().length !== 0) {
+            console.log(text);
+            socket.emit('message', text.val());
+            text.val('');
+        }
+    });
+
+    //Got the message back
+    socket.on('createMessage', message => {
+        $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    });
 });
 
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
 
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream;
-    video.addEventListener('loadmetadata', () => {
-        video.play();
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+});
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
     });
-    videoGrid.append(video);
 };
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+      video.play()
+    });
+    videoGrid.append(video)
+};
+
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollTop(d.prop('scrollHeight'));
+}
+
+const toggleMute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const playStop = () => {
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `<i class="fas fa-microphone"></i><span>Mute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+  
+const setUnmuteButton = () => {
+    const html = `<i class="unmute fas fa-microphone-slash"></i><span>Unmute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `<i class="stop fas fa-video-slash"></i><span>Play Video</span>`
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `<i class="fas fa-video"></i><span>Stop Video</span>`
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
